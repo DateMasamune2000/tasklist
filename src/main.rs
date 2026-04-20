e31d60d89a500eca3a3d1f50e4208b04314d132b
Varun Girimaji <vgirimaji@gmail.com>
2026-04-19 19:27:02 -0500

    Path checks work

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,74 @@
+use dirs;
+use std::path::PathBuf;
+use std::path::Path;
+
 mod migrations;
 mod task;
 
 fn main() {
-	let connection = sqlite::open(":memory:").unwrap();
+    let mut path = match dirs::config_dir() {
+        None => {
+            println!("Cannot find config directory. Using workdir.");
+            PathBuf::new()
+        },
 
-	connection.execute(migrations::CREATE_TABLE).unwrap();
+        Some(x) => x
+    };
 
-	let mut tasks: Vec<task::Task> = vec![];
+    path.push("tasklist");
 
-	let query = "SELECT * FROM tasks;";
-	connection.iterate(query, |vals| {
-		let mut tb = task::TaskBuilder::new();
+    if !path.exists() {
+        println!("path does not exist");
+        std::fs::create_dir(&path);
+    }
 
-		for &(key, value) in vals.iter() {
-			match key {
-				"id" => tb.set_id(value.unwrap().parse::<u64>().expect("u64 id")),
-				"project" => tb.set_project(String::from(value.unwrap())),
-				"task" => tb.set_task(String::from(value.unwrap())),
-				"deadline" => tb.set_deadline(match value {
-					None => None,
-					Some(x) => Some(x.parse::<u64>().expect("u64 timestamp for deadline"))
-				}),
-				&_ => panic!("unsupported column found in table")
-			};
+    path.push("tasks.db");
 
-		}
-		tasks.push(tb.build().expect("all required columns done"));
-		true
-	})
-	.unwrap();
+    let connection = sqlite::open(path).unwrap();
 
-	println!("{:?}", tasks);
+    match connection.execute(migrations::TEST_QUERY) {
+        Ok(_) => {},
+        Err(_) => {
+            println!("DB does not exist. Creating table...");
+            connection.execute(migrations::CREATE_TABLE).unwrap();
+        }
+    };
+
+    connection.execute(migrations::SAMPLE_ENTRIES).unwrap();
+
+    let mut tasks: Vec<task::Task> = vec![];
+
+    let query = "SELECT * FROM tasks;";
+
+    let result = connection.iterate(query, |vals| {
+        let mut tb = task::TaskBuilder::new();
+
+        for &(key, value) in vals.iter() {
+            match key {
+                "id" => tb.set_id(value
+                    .unwrap()
+                    .parse::<u64>()
+                    .expect("u64 id")),
+
+                "project" => tb.set_project(
+                    String::from(value.unwrap())),
+
+                "task" => tb.set_task(
+                    String::from(value.unwrap())),
+
+                "deadline" => tb.set_deadline(match value {
+                    None => None,
+                    Some(x) => Some(x.parse::<u64>()
+                        .expect("u64 timestamp for deadline"))
+                }),
+
+                &_ => panic!("unsupported column found in table")
+            };
+
+        }
+        tasks.push(tb.build().expect("all required columns done"));
+        true
+    });
+
+    println!("{:?}", tasks);
 }
--- src/migrations.rs
+++ src/migrations.rs
@@ -1,6 +1,10 @@
 pub const CREATE_TABLE: &str = "
 CREATE TABLE tasks(id INTEGER PRIMARY KEY, project TEXT, task TEXT, deadline INTEGER);
+";
+
+pub const SAMPLE_ENTRIES: &str = "
 INSERT INTO tasks (project, task, deadline) VALUES ('dank memes', 'make dank meme', 420);
 INSERT INTO tasks (project, task, deadline) VALUES ('dank memes', 'get cancelled', 67);
 ";
 
+pub const TEST_QUERY: &str = "SELECT COUNT(*) FROM tasks;";
